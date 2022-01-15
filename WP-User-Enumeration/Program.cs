2686cfde12717828f3cc457e1a40bc573143a9ed
Her Mes <hermi1312@protonmail.com>
2022-01-15 22:46:03 +0100

    refactor

--- WP-User-Enumeration/Program.cs
+++ WP-User-Enumeration/Program.cs
@@ -1,26 +1,21 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.IO;
+using System.Linq;
 using System.Net;
-using System.Runtime.InteropServices;
-using System.Runtime.InteropServices.ComTypes;
-using System.Security.Cryptography.X509Certificates;
-using System.Text.Json;
-using System.Threading;
-using System.Threading.Channels;
 using Newtonsoft.Json;
 using Newtonsoft.Json.Linq;
 
 namespace WP_User_Enumeration
 {
-    class Program
+    internal class Program
     {
-        static void Main(string[] args)
+        private static void Main(string[] args)
         {
-            Enumerator();   
+            Enumerator();
         }
 
-        static void Enumerator()
+        private static void Enumerator()
         {
             while (true)
             {
@@ -28,7 +23,6 @@ namespace WP_User_Enumeration
 
                 Uri url;
                 string path, urlString;
-                int maxFails, failsCounter = 0, idCounter = 1;
                 var resultList = new List<string>();
 
                 Console.WriteLine("\n|==========| Wordpress Username Enumerator |==========|\n");
@@ -37,34 +31,8 @@ namespace WP_User_Enumeration
                 urlString = Console.ReadLine();
 
                 if (urlString[..4] != "http") urlString = "http://" + urlString;
-
                 url = new Uri(urlString);
 
-                using (var checkVulnerability = new WebClient())
-                {
-                    try
-                    {
-                        checkVulnerability.DownloadString(new Uri(url, "/wp-json/wp/v2/users/"));
-                    }
-                    catch (WebException ex)
-                    {
-                        if (ex.Status == WebExceptionStatus.ProtocolError && ex.Response != null)
-                        {
-                            var resp = (HttpWebResponse) ex.Response;
-
-                            if (resp.StatusCode is HttpStatusCode.NotFound or HttpStatusCode.Forbidden) Console.WriteLine($"[!] Website \"{url.AbsoluteUri}\" is not vulnerable to this scan!");
-                        }
-
-                        else
-                            Console.WriteLine($"[!] Error: {ex.Message}");
-
-                        Console.ReadKey();
-                        Enumerator();
-                    }
-                }
-
-                Console.Write("Max fails in a row: ");
-                maxFails = Convert.ToInt16(Console.ReadLine());
 
                 Console.Write("Output path: ");
                 path = Console.ReadLine();
@@ -72,37 +40,26 @@ namespace WP_User_Enumeration
 
                 Console.WriteLine("\n[+] Enumerating all users...");
 
-                using (var client = new WebClient())
+                try
                 {
-                    for (;;)
-                    {
-                        if (failsCounter > maxFails) break;
+                    using var client = new WebClient();
+                    var response = client.DownloadString(new Uri(url, @"/wp-json/wp/v2/users/"));
+                    var jobject = (JArray) JsonConvert.DeserializeObject(response);
 
-                        try
-                        {
-                            var response = client.DownloadString(new Uri(url, @$"/wp-json/wp/v2/users/{idCounter}"));
-                            var jobject = (JObject) JsonConvert.DeserializeObject(response);
-                            var uname = jobject["slug"].Value<string>();
-                            resultList.Add(uname);
-                            Console.WriteLine($"[!] Found username \"{uname}\" with id {idCounter}");
-                            idCounter++;
-                            failsCounter = 0;
-                        }
+                    if (jobject != null && jobject.Count > 0)
+                        resultList.AddRange(jobject.Select(token => token["slug"].Value<string>()));
+                }
 
-                        catch (Exception e)
-                        {
-                            Console.WriteLine($"[i] {e.Message}");
-                            failsCounter++;
-                            idCounter++;
-                        }
-
-                        Thread.Sleep(10);
-                    }
+                catch (Exception e)
+                {
+                    Console.WriteLine($"[!] ERROR: {e.Message}");
+                    Console.WriteLine("This website might not be vulnerable to this scan! Click any key...");
+                    Console.ReadKey();
                 }
 
                 if (resultList.Count > 0) File.WriteAllLines(path, resultList);
 
-                Console.WriteLine($"\r\n[!] Complete! Found {resultList.Count} usernames");
+                Console.WriteLine($"\r\n[!] Complete! Found {resultList.Count} usernames. Click any key...");
                 Console.ReadKey();
             }
         }
