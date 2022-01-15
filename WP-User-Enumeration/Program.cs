5170f21a6114dda7573717f6daa45d2b34d54022
Her Mes <hermi1312@protonmail.com>
2022-01-15 21:56:32 +0100

    looped

--- WP-User-Enumeration/Program.cs
+++ WP-User-Enumeration/Program.cs
@@ -4,6 +4,7 @@ using System.IO;
 using System.Net;
 using System.Runtime.InteropServices;
 using System.Runtime.InteropServices.ComTypes;
+using System.Security.Cryptography.X509Certificates;
 using System.Text.Json;
 using System.Threading;
 using System.Threading.Channels;
@@ -16,62 +17,94 @@ namespace WP_User_Enumeration
     {
         static void Main(string[] args)
         {
-            Uri url;
-            string path, urlString;
-            int maxFails, failsCounter = 0, idCounter = 1;
-            var resultList = new List<string>();
+            Enumerator();   
+        }
 
-            Console.WriteLine("\n|==========| Wordpress Username Enumerator |==========|\n");
-
-            Console.Write("Target URL: ");
-            urlString = Console.ReadLine();
-
-            if (urlString[..4] != "http")
-                urlString = "http://" + urlString;
-
-            url = new Uri(urlString);
-
-            Console.Write("Max fails in a row: ");
-            maxFails = Convert.ToInt16(Console.ReadLine());
-
-            Console.Write("Output path: ");
-            path = Console.ReadLine();
-
-
-            Console.WriteLine("\n[+] Enumerating all users...");
-
-            using (var client = new WebClient())
+        static void Enumerator()
+        {
+            while (true)
             {
-                for (;;)
-                {
-                    if (failsCounter > maxFails) break;
+                Console.Clear();
 
+                Uri url;
+                string path, urlString;
+                int maxFails, failsCounter = 0, idCounter = 1;
+                var resultList = new List<string>();
+
+                Console.WriteLine("\n|==========| Wordpress Username Enumerator |==========|\n");
+
+                Console.Write("Target URL: ");
+                urlString = Console.ReadLine();
+
+                if (urlString[..4] != "http") urlString = "http://" + urlString;
+
+                url = new Uri(urlString);
+
+                using (var checkVulnerability = new WebClient())
+                {
                     try
                     {
-                        var response = client.DownloadString(new Uri(url, @$"/wp-json/wp/v2/users/{idCounter}"));
-                        var jobject = (JObject)JsonConvert.DeserializeObject(response);
-                        var uname = jobject["slug"].Value<string>();
-                        resultList.Add(uname);
-                        Console.WriteLine($"[!] Found username \"{uname}\" with id {idCounter}");
-                        idCounter++;
-                        failsCounter = 0;
+                        checkVulnerability.DownloadString(new Uri(url, "/wp-json/wp/v2/users/"));
                     }
-
-                    catch (Exception e)
+                    catch (WebException ex)
                     {
-                        Console.WriteLine($"[i] {e.Message}");
-                        failsCounter++;
-                        idCounter++;
+                        if (ex.Status == WebExceptionStatus.ProtocolError && ex.Response != null)
+                        {
+                            var resp = (HttpWebResponse) ex.Response;
+
+                            if (resp.StatusCode is HttpStatusCode.NotFound or HttpStatusCode.Forbidden) Console.WriteLine($"[!] Website \"{url.AbsoluteUri}\" is not vulnerable to this scan!");
+                        }
+
+                        else
+                            Console.WriteLine($"[!] Error: {ex.Message}");
+
+                        Console.ReadKey();
+                        Enumerator();
                     }
-
-                    Thread.Sleep(10);
                 }
+
+                Console.Write("Max fails in a row: ");
+                maxFails = Convert.ToInt16(Console.ReadLine());
+
+                Console.Write("Output path: ");
+                path = Console.ReadLine();
+
+
+                Console.WriteLine("\n[+] Enumerating all users...");
+
+                using (var client = new WebClient())
+                {
+                    for (;;)
+                    {
+                        if (failsCounter > maxFails) break;
+
+                        try
+                        {
+                            var response = client.DownloadString(new Uri(url, @$"/wp-json/wp/v2/users/{idCounter}"));
+                            var jobject = (JObject) JsonConvert.DeserializeObject(response);
+                            var uname = jobject["slug"].Value<string>();
+                            resultList.Add(uname);
+                            Console.WriteLine($"[!] Found username \"{uname}\" with id {idCounter}");
+                            idCounter++;
+                            failsCounter = 0;
+                        }
+
+                        catch (Exception e)
+                        {
+                            Console.WriteLine($"[i] {e.Message}");
+                            failsCounter++;
+                            idCounter++;
+                        }
+
+                        Thread.Sleep(10);
+                    }
+                }
+
+                if (resultList.Count > 0) File.WriteAllLines(path, resultList);
+
+                Console.WriteLine($"\r\n[!] Complete! Found {resultList.Count} usernames");
+                Console.ReadKey();
             }
-
-            if(resultList.Count > 0)
-                File.WriteAllLines(path, resultList);
-
-            Console.WriteLine($"\r\n[!] Complete! Found {resultList.Count} usernames");
         }
     }
 }
